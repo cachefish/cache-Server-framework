55114457dc4c82fd7774a0a137f1866145638568
cachefish <780207419@qq.com>
2020-06-15 22:45:52 -0700

    读写操作

--- src/SocketIO.cpp
+++ src/SocketIO.cpp
@@ -1,135 +1,110 @@
-#include"SocketIO.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<fcntl.h>
-#include<errno.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#define ERR_EXIT(m)\
-    do{     \
-        perror(m);      \
-        exit(EXIT_FAILURE);     \
-    }while (0)
-    
-namespace cf
-{
-SocketIO::SocketIO(int fd)
-:fd_(fd)
-,cnt_(0)
-,ptr_(buffer_)
-{
-    ::memset(buffer_,0,kBufferSize);
-}
+#include "SocketIO.h"
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
 
-ssize_t SocketIO::readn(char *usrbuf,size_t count)
+ssize_t SocketIO::readn(char *buf, size_t count)
 {
-    size_t nleft=count ;    //剩余的字节数
-    ssize_t nread;  //返回值
-    char*pbuf=usrbuf;//缓冲区的偏移量
-    
-    while (nleft>0)
+    size_t nleft = count;  //剩余的字节数
+    ssize_t nread; //用作返回值
+    char *bufp = (char*)buf; //缓冲区的偏移量
+
+    while(nleft > 0)
     {
-        nread=this->read(pbuf,nleft);
-        if((nread=this->read(pbuf,nleft))==-1)
-        {
-            return -1;
-        }
-        else if(nread==0)
-        {
-            break;
-        }
-        nleft-=nread;
-        pbuf+=nread;
-    }
-    return (count-nleft);// 例如，你想读取10个字节，但可能可读的就只有6个字节
-                            // 因为可能内核缓冲区中就只有6个字节
-                            // 通过this->read读到用户缓冲区也就只有6个字节,并返回0
-                            // 那么readn也就只能读6个字节
-}
-ssize_t SocketIO::readLine(char*usrbuf,size_t maxlen)
-{
-    int i;
-    int nread;
-    char*pbuf = usrbuf;
-    char c;
-    for(i=0;i<maxlen-1;++i)
-    {
-        if((nread=this->read(&c,1))==-1) // ERROR
-        {
-            return -1;
-        }
-        else if(nread==0)// 第一个字符读取遇到EOF
-        {
-            if(i==0)
-            {
-                return 0;
-            }
-            break;
-        }
-        *pbuf++=c;// 放入usrbuf
-        if(c=='\n') // 碰到换行符直接退出循环
-            break;
-    }
-    *pbuf='\0';
-    return i;// 返回成功读取的字符数，不包括最后的'\0'
-}
-ssize_t SocketIO::writen(int fd,const void *buf,size_t count)
-{
-    size_t nleft = count;
-    ssize_t nwrite;
-    const char *pbuf=static_cast<const char*>(buf);
-    while(nleft>0)
-    {
-        if(nwrite=write(fd,pbuf,nleft)<=0)
-        {
-            if(nwrite==-1&&errno==EINTR)
-                continue;
-            return -1;
-        }
-        nleft-=nwrite;
-        pbuf+=nwrite;
-    }
-    return count;
-}
-ssize_t SocketIO::read(char *usrbuf,size_t n)
-{
-// 缓冲区为空，从内核缓冲区中read到用户缓冲区buffer_
-    // 注意从内核缓冲区未必就一定能读到kBufferSize个字节
-    while(cnt_ <= 0)
-    {
-        ssize_t nread =
-            ::read(fd_, buffer_, kBufferSize);
-        
+        nread = ::read(sockfd_, bufp, nleft);
         if(nread == -1)
         {
             if(errno == EINTR)
-                continue; 
-            return -1;       // ERROR
+                continue;
+            return -1; // ERROR
         }
-        else if(nread == 0)  // 0 表示读到了末尾，
-            return 0;        // 在socket中表示对方已经关闭了连接，这里接收到一个FIN请求，从而返回0
+        else if(nread == 0) //EOF
+            break;
 
-        // 正常读取
-        cnt_ = nread;
-        ptr_ = buffer_; //每次从内核缓冲区中read，需要重置指针
+        nleft -= nread;
+        bufp += nread;
     }
 
-    // 现有库存和用户要求的数量 取较小者
-    int cnt = (cnt_ < n) ? cnt : n;
-    ::memcpy(usrbuf, ptr_, cnt);  // 从用户缓冲区buffer_中读取cnt个字节到用户传进来的usrbuf中
-    ptr_ += cnt; // buffer_缓冲区指针往后移cnt个位置
-    cnt_ -= cnt; // buffer_缓冲区可用字节数减少cnt个
-
-    return cnt;  // 返回成功读取的字节数 
-} 
-size_t SocketIO::recv_peek(char * buf, size_t count)
-{
-	int nread;
-	do
-	{
-		nread = ::recv(fd_, buf, count, MSG_PEEK);
-	}while(nread == -1 && errno == EINTR);
-	return nread;
+    return (count - nleft);
 }
+
+ssize_t SocketIO::writen(const char *buf, size_t count)
+{
+    size_t nleft = count;
+    ssize_t nwrite;
+    const char *bufp = buf;
+    
+    while(nleft > 0)
+    {
+        nwrite = ::write(sockfd_, bufp, nleft);
+        if(nwrite <= 0) // ERROR
+        {
+            if(nwrite == -1 && errno == EINTR)
+                continue;
+            return -1;
+        }
+
+        nleft -= nwrite;
+        bufp += nwrite;
+    }
+    
+    return count;
+}
+
+//预览内核缓冲区数据
+ssize_t SocketIO::recv_peek(char *buf, size_t len)
+{
+    int nread;
+    do
+    {
+        nread = ::recv(sockfd_, buf, len, MSG_PEEK);
+    }
+    while(nread == -1 && errno == EINTR);
+
+    return nread;
+}
+
+
+
+ssize_t SocketIO::readline(char *usrbuf, size_t maxlen)
+{
+    //
+    size_t nleft = maxlen - 1;
+    char *bufp = usrbuf; //缓冲区位置
+    size_t total = 0; //读取的字节数
+
+    ssize_t nread;
+    while(nleft > 0)
+    {
+        //预读取
+        nread = recv_peek(bufp, nleft);
+        if(nread <= 0)
+            return nread;
+
+        //检查\n
+        int i;
+        for(i = 0; i < nread; ++i)
+        {
+            if(bufp[i] == '\n')
+            {
+                //找到\n
+                size_t nsize = i+1;
+                if(readn(bufp, nsize) != static_cast<ssize_t>(nsize))
+                    return -1;
+                bufp += nsize;
+                total += nsize;
+                *bufp = 0;
+                return total;
+            }
+        }
+
+        //没找到\n
+        if(readn(bufp, nread) != nread)
+            return -1;
+        bufp += nread;
+        total += nread;
+        nleft -= nread;
+    }
+    *bufp = 0;
+    return maxlen - 1;
 }
--- src/SocketIO.h
+++ src/SocketIO.h
@@ -1,31 +1,26 @@
 #ifndef _SOCKETIO_H_
 #define _SOCKETIO_H_
 #include"NonCopyable.h"
-#include<unistd.h>
+#include <sys/types.h>
 
 namespace cf
 {
-    class SocketIO
+class SocketIO : NonCopyable
+{
+public: 
+    explicit SocketIO(int sockfd)
+    : sockfd_(sockfd)
     {
-        public:
-            explicit SocketIO(int fd);
+    }
 
-            ssize_t readn(char *usrbuf,size_t count);
-            ssize_t readLine(char*usrbuf,size_t maxlen);
-            ssize_t writen(int fd,const void *buf,size_t count);
+    ssize_t readn(char *buf, size_t count);
+    ssize_t writen(const char *buf, size_t count);
+    ssize_t readline(char *usrbuf, size_t maxlen);
+private:
+    ssize_t recv_peek(char *buf, size_t len);
 
-        private:
-            ssize_t read(char *usrbuf,size_t n);
-            size_t recv_peek(char * buf, size_t count);
-            static const int kBufferSize = 65536;
-        private:
-            int fd_;                 //fd
-            int cnt_;               //缓冲区可用的字节数
-            char*ptr_;           //指向缓冲区可用的第一个字节
-            char buffer_[kBufferSize];          //缓冲区
-    };
-
-
+    const int sockfd_;
+};
 
 }
-#endif
+#endif //SOCKETIO_H_
